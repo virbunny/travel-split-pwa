d5ba0435db16b1da43f74cfa8e57952e157c8365
virbunny <virbunny@gmail.com>
2026-08-27 23:19:29 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mitu-travel-split-v1-4-6-unified-expense-details';
+const CACHE_NAME = 'mitu-travel-split-v1-4-7-ledger-inline-edit';
 const ASSETS = [
   './',
   './index.html',
